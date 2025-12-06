448d3a1a1bc375d43a97f2fc7dddd2d3dfa9bda1
Mayank Goyal <imtrinity94@gmail.com>
2025-12-06 18:16:43 +0530

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "swagger.yaml",
+    url: "https://raw.githubusercontent.com/imtrinity94/vmware_vro/refs/heads/master/vRO%208.18.1%20Swagger%20OpenAPI.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
