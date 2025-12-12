7063516d1e8d34aeac1584e39b4a9f9df0c41ca6
Mayank Goyal <imtrinity94@gmail.com>
2025-12-12 19:02:31 +0530

    Update Swagger UI configuration for multiple URLs

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,11 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "vRO 8.18.1 Swagger OpenAPI.json",
+    // url: "vRO 8.18.1 Swagger OpenAPI.json",
+     urls: [
+      { url: "vRO 8.18.1 Swagger OpenAPI.json", name: "Aria Automation Orchestrator 8.18.1" },
+      { url: "VCFOO 9.0.0 Swagger OpenAPI.json", name: "VCF Operations Orchestrator 9.0.0" }
+    ],
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
